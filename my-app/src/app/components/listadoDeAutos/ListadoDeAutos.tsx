1b16722bbfd7ef179d762b0e5df14d3f269a8044
JhairCJ <jhaircjxd@gmail.com>
2025-04-26 02:50:10 -0400

    fix: rollback de bugs severos

--- my-app/src/app/components/listadoDeAutos/ListadoDeAutos.tsx
+++ my-app/src/app/components/listadoDeAutos/ListadoDeAutos.tsx
@@ -2,7 +2,7 @@
 import React, { useState } from 'react';
 import styles from './ListadoDeAutos.module.css';
 import ModalDeConfirmacion from '@components/modal/ModalDeConfirmacion';
-import { useRouter } from 'next/router';
+import { useRouter } from 'next/navigation';
 import { API_URL } from '@config/api';
 
 // Interfaces para las solicitudes y autos
@@ -217,7 +217,7 @@ const ListadoDeAutos: React.FC<ListadoDeAutosProps> = ({ activeFilter, autos = [
       // En un caso real, lo ideal sería notificar al componente padre para que recargue los datos
       console.log(`Solicitud ${solicitudSeleccionada} procesada para el auto ${autoSeleccionado}`);
     }
-    router.reload();
+    window.location.reload();
   };
 
   // Si no hay autos disponibles en absoluto
