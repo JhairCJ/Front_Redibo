28eb61a6ac1dbd69e351783679f495eefb33c448
JhairCJ <jhaircjxd@gmail.com>
2025-05-07 21:52:11 -0400

    fix: eliminar boton de pago del 50% descartado por el Product Owner

--- my-app/src/app/components/auto/PrecioAuto.tsx
+++ my-app/src/app/components/auto/PrecioAuto.tsx
@@ -86,26 +86,6 @@ export default function PrecioAuto({
         >
           Pagar el 100% ahora
         </button>
-        <button
-          onClick={() => setIsPagoModalOpen(true)}
-          className="w-full border border-gray-300 bg-azul-oscuro py-3 rounded-md font-medium flex items-center justify-center transition"
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={1.5}
-            stroke="currentColor"
-            className="size-6"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M2.25 8.25h19.5M2.25 9h19.5m-16.5 5.25h6m-6 2.25h3m-3.75 3h15a2.25 2.25 0 0 0 2.25-2.25V6.75A2.25 2.25 0 0 0 19.5 4.5h-15a2.25 2.25 0 0 0-2.25 2.25v10.5A2.25 2.25 0 0 0 4.5 19.5Z"
-            />
-          </svg>
-          Pagar 50% ahora
-        </button>
       </div>
       <PasarelaDePago
         isOpen={isPagoModalOpen}
